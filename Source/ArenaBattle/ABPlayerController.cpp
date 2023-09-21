26abdd0c12e95f627924640346960637ae59a0a5
jaehuru <kimbs0516@gmail.com>
2023-09-22 03:50:36 +0900

    Sep_22

---
+++ Source/ArenaBattle/ABPlayerController.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ABPlayerController.h"
+
+void AABPlayerController::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+	ABLOG_S(Warning);
+}
+
+void AABPlayerController::OnPossess(APawn *aPawn)
+{
+	Super::OnPossess(aPawn);
+	ABLOG_S(Warning);
+}
+
+void AABPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FInputModeGameOnly InputMode;
+	SetInputMode(InputMode);
+}
+
+
