9a71a40350fb40643728f8ef1d02cb992e4a1516
jaehuru <kimbs0516@gmail.com>
2023-10-06 01:59:09 +0900

    Oct_06

---
+++ Source/ArenaBattle/private/ABItemBox.cpp
@@ -0,0 +1,70 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ABItemBox.h"
+#include "ABWeapon.h"
+#include "ABCharacter.h"
+
+// Sets default values
+AABItemBox::AABItemBox()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	Trigger = CreateDefaultSubobject<UBoxComponent>(TEXT("TRIGGER"));
+	Box = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BOX"));
+
+	RootComponent = Trigger;
+	Box->SetupAttachment(RootComponent);
+
+	Trigger->SetBoxExtent(FVector(40.0f, 42.0f, 30.0f));
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> SM_BOX(TEXT("/Script/Engine.StaticMesh'/Game/InfinityBladeGrassLands/Environments/Breakables/StaticMesh/Box/SM_Env_Breakables_Box1.SM_Env_Breakables_Box1'"));
+	if (SM_BOX.Succeeded())
+	{
+		Box->SetStaticMesh(SM_BOX.Object);
+	}
+
+	Box->SetRelativeLocation(FVector(0.0f, -3.5f, -30.0f));
+
+	Trigger->SetCollisionProfileName(TEXT("ItemBox"));
+	Box->SetCollisionProfileName(TEXT("NoCollision"));
+
+	WeaponItemClass = AABWeapon::StaticClass();
+}
+
+// Called when the game starts or when spawned
+void AABItemBox::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AABItemBox::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+	Trigger->OnComponentBeginOverlap.AddDynamic(this, &AABItemBox::OnCharacterOverlap);
+}
+
+void AABItemBox::OnCharacterOverlap(UPrimitiveComponent* overlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	ABLOG_S(Warning);
+
+	auto ABCharacter = Cast<AABCharacter>(OtherActor);
+	ABCHECK(nullptr != ABCharacter);
+
+	if (nullptr != ABCharacter && nullptr != WeaponItemClass)
+	{
+		if (ABCharacter->CanSetWeapon())
+		{
+			auto NewWeapon = GetWorld()->SpawnActor<AABWeapon>(WeaponItemClass, FVector::ZeroVector, FRotator::ZeroRotator);
+			ABCharacter->SetWeapon(NewWeapon);
+		}
+		else
+		{
+			ABLOG(Warning, TEXT("%s can't equip weapon currently."), * ABCharacter->GetName());
+		}
+	}
+}
+
+
+
